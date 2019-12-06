5de7c49a94b631940b8ca23b8bf80df153e94392
Sergey Pogorelov <pogorelov.s.y@gmail.com>
2019-12-06 15:53:30 +0200

    Option to build executable jar

--- src/main/kotlin/com/pogorelovs/kafka_producer/MyKafkaProducer.kt
+++ src/main/kotlin/com/pogorelovs/kafka_producer/MyKafkaProducer.kt
@@ -4,34 +4,7 @@ import org.apache.kafka.clients.producer.KafkaProducer
 import org.apache.kafka.clients.producer.Producer
 import org.apache.kafka.clients.producer.ProducerRecord
 import org.apache.kafka.common.serialization.StringSerializer
-import java.io.File
-import java.nio.file.Files
-import java.nio.file.Path
 import java.util.*
-import kotlin.system.exitProcess
-
-fun main(args: Array<String>) {
-
-    if (args.size < 3) {
-        println("Usage: $0 broker topic pathToInputFile")
-        println("Example: $0 localhost:9092 test input")
-        exitProcess(1)
-    }
-
-    val broker = args[0]
-    val topic = args[1]
-    var filePath = args[2]
-
-    if (filePath.startsWith("~" + File.separator)) {
-        filePath = System.getProperty("user.home") + filePath.substring(1)
-    }
-
-    val producer = MyKafkaProducer(broker, topic)
-
-    Files.lines(Path.of(filePath)).forEach {
-        producer.produce(it)
-    }
-}
 
 class MyKafkaProducer(brokers: String, private val topic: String) {
     private val producer = createProducer(brokers)
